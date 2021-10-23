8a13d3800a04bcbd625b083a42b3f2f482f25e37
G <garyyansen@gmail.com>
2021-10-23 22:04:23 +0800

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,29 @@
-CREATE TABLE EMPLOYEE(
-TEXT ename
-TEXT email
-TEXT contact
-DATE resignDate
-INT eid PRIMARY KEY);
+DROP TABLE IF EXISTS Employees, Departments, HealthDeclaration, Sessions
 
+CREATE TABLE Departments (
+    did INTEGER PRIMARY KEY,
+    dname TEXT NOT NULL
+);
 
+CREATE TABLE HealthDeclaration (
+    declareDate DATE,
+    temp NUMERIC NOT NULL, 
+    fever BOOLEAN GENERATED ALWAYS AS (temp > 37.5) STORED, 
+    PRIMARY KEY(eid, declareDate),
+    FOREIGN KEY (eid) REFERENCES Employees(eid) ON DELETE CASCADE ON UPDATE CASCADE,
+    CHECK (temp >= 34.0 AND temp <= 43.0)
+);
+
+CREATE TABLE Sessions (
+    sessionDate DATE,
+    sessionTime INT,
+    room INTEGER, 
+    floor INTEGER,
+    bookerId BIGINT,
+    PRIMARY KEY (sessionDate, sessionTime, room, floor),
+    FOREIGN KEY (room, floor) REFERENCES MeetingRooms(room, floor) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (bookerId) REFERENCES Booker(eid);
+    CHECK ((sessionDate >= CURRENT_DATE) OR (sessionTime >= CURRENT_TIME))
+)
 
 
