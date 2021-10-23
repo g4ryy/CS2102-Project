d18ab3e91063327f3bf6f970137c9cb388a356b0
LAPTOP-C78H35UJ\Evan <hd.kca.ang@gmail.com>
2021-10-23 21:50:50 +0800

    Update commas

--- schema.sql
+++ schema.sql
@@ -1,25 +1,25 @@
 CREATE TABLE Employees(
-TEXT ename
-TEXT email UNIQUE NOT NULL
-TEXT home_contact DEFAULT NULL
-TEXT mobile_contact DEFAULT NULL
-TEXT office_contact DEFAULT NULL
-DATE resignDate
-INT eid PRIMARY KEY
-INT department_id NOT NULL
+TEXT ename,
+TEXT email UNIQUE NOT NULL,
+TEXT home_contact DEFAULT NULL,
+TEXT mobile_contact DEFAULT NULL,
+TEXT office_contact DEFAULT NULL,
+DATE resignDate,
+INT eid PRIMARY KEY,
+INT department_id NOT NULL,
 FOREIGN KEY department_id REFERENCES Departments(did)
 );
 
 CREATE TABLE Departments(
-INT did PRIMARY KEY
-TEXT dname
+INT did PRIMARY KEY,
+TEXT dname,
 );
 
 CREATE TABLE HealthDeclaration(
-DATE declareDate
-TEXT temp
-INT fever
-PRIMARY KEY(eid, declareDate)
+DATE declareDate,
+TEXT temp,
+INT fever,
+PRIMARY KEY(eid, declareDate),
 FOREIGN KEY (eid) REFERENCES Employees(eid) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
