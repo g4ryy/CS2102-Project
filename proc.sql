1f84362dc13772a4a7387f43259eeb8a78360bb6
G <garyyansen@gmail.com>
2021-10-26 19:13:22 +0800

    Update routines

---
+++ proc.sql
@@ -0,0 +1,19 @@
+---------------------------------- Application Functionalities ------------------------------
+
+
+
+
+
+------------------------------------- TRIGGERS ---------------------------------------------
+-- generates unique email for a new employee that has just been added.
+CREATE OR REPLACE FUNCTION generate_email()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE Employees SET email = LOWER(REPLACE(NEW.ename, ' ', ''))  || NEW.eid::TEXT || '@company.com';
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql; 
+
+CREATE TRIGGER new_employee_added 
+AFTER INSERT ON Employees
+FOR EACH ROW EXECUTE FUNCTION generate_email();
